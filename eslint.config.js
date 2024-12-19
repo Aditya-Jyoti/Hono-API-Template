dd23b5f24e9c89ca831108f14e6baacfa874f3b6
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-12-20 03:21:11 +0530

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      '.eslintignore',
+      '.gitignore',
+      'pre-commit',
+      '.prettierignore',
+      'schema.prisma',
+    ],
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 80,
+          tabWidth: 2,
+        },
+      ],
+    },
+  },
+];
